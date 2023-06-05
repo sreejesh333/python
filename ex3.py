69ad569fd82db81e890caadff8f275ae27832330
sreejesh333 <sreejeshravindran523@gmail.com>
2023-06-05 07:20:10 +0530

    solved sucessfully

---
+++ ex3.py
@@ -0,0 +1,22 @@
+print("i will count my chickens") #this is a print statement  
+print("hens", 25.0 + 30 / 6) # this counts the number of hens
+print("Roosters", 100.0 -25 * 3 % 4.0) # this counts the number of roosters
+print("now i will count the number of eggs") #print statement
+
+
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #counts the number of eggs
+
+print("Is it true that 3 + 2 < 5 - 7?") #print statement 
+
+print(3 + 2 < 5 - 7) # some conditional statements
+
+print("What is 3 + 2?", 3 + 2) # adding two numbers
+print("What is 5 - 7?", 5 - 7) # substracting 2 numbers
+
+print("Oh, that's why it's False.") # print statement 
+print("How about some more.") # print statement
+
+print("Is it greater?", 5 > -2) # some conditional statements
+print("Is it greater or equal?", 5 >= -2) #some conditional statements
+print("Is it less or equal?", 5 <= -2)#some conditional statements
---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = 'sreejesh ravindran'
+my_age = 23
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'yelow'
+my_teeth = 'White'
+my_hair = 'Brown' 
+
+print(f"let's talk about {my_name}")
+weight_inkg = my_weight /  2.205
+height_incenti = 74 * 2.54
+print(f"He is {my_height} tall")
+print(f"Height in centimeters {height_incenti} ")
+print(f"He is  {my_weight} pounds heavy")
+print(f"Weight in kg  {weight_inkg} ")
+print("that's not that heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
